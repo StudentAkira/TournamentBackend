322dfc6cce5bea0aeaaa694d6103e8639e94f252
Akira <am.1.00@bk.ru>
2024-03-17 14:33:56 +0300

    deletion implemented

--- routes/team_participant_nomination_event/team_participant_nomination_event_service.py
+++ routes/team_participant_nomination_event/team_participant_nomination_event_service.py
@@ -40,37 +40,6 @@ class TeamParticipantNominationEventService:
             team_participant_nomination_event_data.team_name
         )
         team_participant_nomination_event_data.team_name = team_name
-        self.team_participant_nomination_event_validations(response, token, team_participant_nomination_event_data)
-        self.__team_participant_nomination_event_manager.append_team_participant_nomination_event(
-            team_participant_nomination_event_data
-        )
-        return {"message": self.__team_participant_appended_message}
-
-    def delete_team_participant_nomination_event(
-            self,
-            response: Response,
-            token: str,
-            team_participant_nomination_event_data: DeleteTeamParticipantNominationEventSchema
-    ):
-        team_name = self.get_team_name_from_team_name_or_participant_email(
-            team_participant_nomination_event_data.team_name
-        )
-        team_participant_nomination_event_data.team_name = team_name
-        self.team_participant_nomination_event_validations(response, token, team_participant_nomination_event_data)
-        self.__team_participant_nomination_event_manager.delete_team_participant_nomination_event(
-            team_participant_nomination_event_data
-        )
-        return {"message": self.__team_participant_deleted_message}
-
-    def get_team_name_from_team_name_or_participant_email(self, team_name_or_participant_email):
-        return self.__team_manager.get_team_name_from_team_name_or_participant_email(team_name_or_participant_email)
-
-    def team_participant_nomination_event_validations(
-            self,
-            response: Response,
-            token: str,
-            team_participant_nomination_event_data
-        ):
         decoded_token = self.__token_manager.decode_token(token, response)
         team_name = team_participant_nomination_event_data.team_name
         nomination_name = team_participant_nomination_event_data.nomination_name
@@ -87,3 +56,38 @@ class TeamParticipantNominationEventService:
             nomination_name,
             event_name
         )
+        self.__team_participant_nomination_event_manager.append_team_participant_nomination_event(
+            team_participant_nomination_event_data
+        )
+        return {"message": self.__team_participant_appended_message}
+
+    def delete_team_participant_nomination_event(
+            self,
+            response: Response,
+            token: str,
+            team_participant_nomination_event_data: DeleteTeamParticipantNominationEventSchema
+    ):
+        team_name = self.get_team_name_from_team_name_or_participant_email(
+            team_participant_nomination_event_data.team_name
+        )
+        team_participant_nomination_event_data.team_name = team_name
+
+        decoded_token = self.__token_manager.decode_token(token, response)
+        team_name = team_participant_nomination_event_data.team_name
+        nomination_name = team_participant_nomination_event_data.nomination_name
+        event_name = team_participant_nomination_event_data.event_name
+        participant_email = team_participant_nomination_event_data.participant_email
+        self.__validator.raise_exception_if_registration_finished(nomination_name, event_name)
+        self.__team_manager.raise_exception_if_not_found(team_name)
+        self.__participant_manager.raise_exception_if_not_found(participant_email)
+        self.__nomination_event_manager.raise_exception_if_not_found(nomination_name, event_name)
+        self.__validator.validate_user_entity_ownership(decoded_token, team_name, event_name)
+        self.__validator.raise_exception_if_participants_not_in_team(team_name, participant_email)
+
+        self.__team_participant_nomination_event_manager.delete_team_participant_nomination_event(
+            team_participant_nomination_event_data
+        )
+        return {"message": self.__team_participant_deleted_message}
+
+    def get_team_name_from_team_name_or_participant_email(self, team_name_or_participant_email):
+        return self.__team_manager.get_team_name_from_team_name_or_participant_email(team_name_or_participant_email)
